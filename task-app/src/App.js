331eb2736d4157cd286ee7953d4acfbdf21513a1
djk01281 <djk01281@gmail.com>
2022-11-24 15:44:42 +0000

    feat: add task submit mockup

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 import Overview from "./components/Overview.js";
+import { useState } from "react";
+
 function App() {
-  return <Overview tasks={[1, 2, 3]} />;
+  const [taskState, setTasks] = useState([1, 2, 3]);
+  const [inputText, setText] = useState("");
+
+  return (
+    <div>
+      <Input inputText={inputText} setText={setText}></Input>
+      <div class="Overview-mockup">{inputText}</div>
+      <Overview tasks={taskState} />
+    </div>
+  );
 }
 
+function Input(props) {
+  const { inputText, setText } = props;
+
+  return (
+    <div>
+      <input type="text" id="taskInput" name="taskInput"></input>
+      <Button
+        onClick={() => {
+          setText("mockup");
+        }}
+      ></Button>
+    </div>
+  );
+}
+function Button(props) {
+  return <button onClick={props.onClick}></button>;
+}
 export default App;
